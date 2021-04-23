feb5f922dbbb678651589c3ab0e876b8850ffc27
david <davidmonarrez3@gmail.com>
2021-04-23 10:57:00 -0700

    'upload'

--- src/App.js
+++ src/App.js
@@ -1,23 +1,76 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
+
+  const colors = ['red', 'blue', 'yellow', 'white'];
+  const boxes = [];
+  let columnNumber = 0;
+  
+const getRandomInt = (max)=> {
+  return parseInt(Math.floor(Math.random() * Math.floor(max)));
+}
+
+const createBoxesReverse = () => {
+  for (let i = 1; i < 11; i++) {
+    if( i % 2 === 0 && columnNumber <= 10) {
+      columnNumber = columnNumber + 1;
+      boxes.push(
+        <div className={`box ${colors[getRandomInt(4)]}`} onClick={(e)=>{
+          e.target.className = `box ${colors[getRandomInt(4)]} ${(getRandomInt(2) === 0)? "rotate" : console.log("no rotation")}`;}}> 
+          <div className = 'vertical-line'></div> 
+        </div>
+      );
+    } else {
+      boxes.push(
+      <div className={`box ${colors[getRandomInt(4)]}`} onClick={(e)=>{
+        e.target.className = `box ${colors[getRandomInt(4)]} ${(getRandomInt(2) === 0)? "rotate" : console.log("no rotation")}`;}}> 
+        <div className = 'horizontal-line'></div> 
+      </div>);
+    }
+  }
+}
+
+const createBoxes = () => {
+  for (let i = 1; i < 11; i++) {
+    if (i % 2 === 0 && columnNumber <= 10) {
+      columnNumber = columnNumber + 1;
+      boxes.push(
+        <div className={`box ${colors[getRandomInt(4)]}`} onClick={(e)=>{
+          e.target.className = `box ${colors[getRandomInt(4)]} ${(getRandomInt(2) === 0)? "rotate" : console.log("no rotation")}`; }}> 
+          <div className = 'horizontal-line'></div> 
+        </div>
+      );
+    } else {
+      boxes.push(
+        <div className={`box ${colors[getRandomInt(4)]}`} onClick={(e)=>{
+          e.target.className = `box ${colors[getRandomInt(4)]} ${(getRandomInt(2) === 0)? "rotate" : console.log("no rotation")}`;}}> 
+          <div className = 'vertical-line'></div> 
+        </div>
+      );
+    }
+  }
+}
+
+const generateBoxes = () => {
+  for (let i = 1; i < 7; i++) {
+    if (i % 2 === 0) {
+      createBoxes();
+      columnNumber = 0;
+    } else {
+      createBoxesReverse();
+      columnNumber = 0;
+    }
+  }
+}
+
+generateBoxes();
+
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className='grid'>
+        {boxes}
+      </div>
     </div>
   );
 }
